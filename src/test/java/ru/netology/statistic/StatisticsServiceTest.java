c86602d10f5c0e7d6ab5bb80f41d0e77c98597f6
VKartsev <vach-partner@mail.ru>
2022-06-25 23:29:30 +0300

    Final version of homework. Extended tests, updated pom.xml

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -9,13 +9,18 @@ public class StatisticsServiceTest {
     @Test
     public void findMax() {
         StatisticsService service = new StatisticsService();
-
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-
         long expected = 12;
         long actual = service.findMax(incomesInBillions);
 
-
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void findMaxAgain() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {3, 4};
+        long expected = 4;
+        long actual = service.findMax(incomesInBillions);
         Assertions.assertEquals(expected, actual);
     }
 }
